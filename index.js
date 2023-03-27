dbce928878ddcb7a5e4c292a6ab4f6486391a2a1
carol-sf <annacarolina.franca21@gmail.com>
2023-03-27 16:48:07 -0300

    adicionando o numero das fases

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-let campo = document.querySelector('#campo');
-let emAndamento = 0;
-let vencido = 0;
-
-
-
 let labirinto = document.querySelector('.labirinto');
 let inicio = document.querySelector('#inicio');
 let chegada = document.querySelector('#chegada');
 let btnStatus = document.querySelector('#btnStatus');
 let iniciado = 0;
+let tentarNovamente = 1;
+let fase = 1;
 
 labirinto.addEventListener('mouseleave', perdeu);
 inicio.addEventListener('mouseleave', iniciou);
@@ -34,7 +30,8 @@ function venceu() {
         status.style.display = 'block';
         statusTitulo.innerHTML = 'Você venceu!';
         btnStatus.innerHTML = 'Proxima fase';
-        btnStatus.style.background = 'rgb(53, 184, 140)'
+        btnStatus.style.background = 'rgb(53, 184, 140)';
+        tentarNovamente = 0;
     }
 }
 
@@ -48,12 +45,15 @@ function jogar() {
     btnStatus.style.background = 'red'
     iniciado = 0;
 
-    if(btnStatus.innerHTML == 'Tentar novamente') {
+    if(tentarNovamente == 1) {
         let status = document.querySelector('#status');
         status.style.display = 'none';
     } else {
-        console.log('proxima fase');
-        let status = document.querySelector('#status');
+        let tituloFase = document.querySelector('#tituloFase');
+
         status.style.display = 'none';
+        fase++;
+        tituloFase.innerHTML = `Fase ${fase}`;
+        tentarNovamente = 1;
     }
 }
