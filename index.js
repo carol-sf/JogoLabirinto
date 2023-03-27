10d1fdd200b6d302b49c4841fcc2268ca09c4830
carol-sf <annacarolina.franca21@gmail.com>
2023-03-27 12:49:32 -0300

    adicionando a tela de game over

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-var labirinto = document.querySelector('.labirinto');
+let labirinto = document.querySelector('.labirinto');
+let btnTentarNovamente = document.querySelector('#btnTentarNovamente');
 
 labirinto.addEventListener('mouseout', sair);
+btnTentarNovamente.addEventListener('click', tentarNovamente);
 
 function sair() {
+    let status = document.querySelector('#status');
     labirinto.style.background = 'red';
+    status.style.display = 'block';
+}
+
+function tentarNovamente() {
+    let status = document.querySelector('#status');
+    labirinto.style.background = 'white';
+    status.style.display = 'none';
+    
 }
