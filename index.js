287c55a60d9a5e5e6b5ef28a39235f92a5065e7e
carol-sf <annacarolina.franca21@gmail.com>
2023-03-27 13:06:24 -0300

    adicionando div de inicio

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
+let labirintoInicio = document.querySelector('#labirintoInicio');
 let labirinto = document.querySelector('.labirinto');
 let btnTentarNovamente = document.querySelector('#btnTentarNovamente');
+let jogoIniciado = 0;
 
+labirintoInicio.addEventListener('mouseenter', iniciarJogo)
 labirinto.addEventListener('mouseout', sair);
 btnTentarNovamente.addEventListener('click', tentarNovamente);
 
+function iniciarJogo() {
+    jogoIniciado = 1;
+}
+
 function sair() {
-    let status = document.querySelector('#status');
-    labirinto.style.background = 'red';
-    status.style.display = 'block';
+    if (jogoIniciado == 1) {
+        let status = document.querySelector('#status');
+        labirinto.style.background = 'red';
+        status.style.display = 'block';
+    }
 }
 
 function tentarNovamente() {
     let status = document.querySelector('#status');
-    labirinto.style.background = 'white';
     status.style.display = 'none';
-    
+    labirinto.style.background = 'white';
+    jogoIniciado = 0;
 }
